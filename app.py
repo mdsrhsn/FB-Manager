433c99d7c7b2a21d857228f529c36429efbeb1a8
mdsrhsn <mdsr.h01@gmail.com>
2026-05-13 22:44:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -534,8 +534,10 @@ def init_db_command():
     init_db(app)
 
 
-if __name__ == '__main__':
+with app.app_context():
+    db.create_all()
     init_db(app)
+
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
-    
+    app.run(host='0.0.0.0', port=port)
