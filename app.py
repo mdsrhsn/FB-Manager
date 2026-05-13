7dfce3a1ed36d7df5986ba508a76d43222633c2b
mdsrhsn <mdsr.h01@gmail.com>
2026-05-13 22:35:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -538,8 +538,4 @@ if __name__ == '__main__':
     init_db(app)
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
-    if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-
-    app.run(host="0.0.0.0", port=5000)
+    
